622421a7383d9d1ff919861d11ebc9c0ca629dbb
Sara Ahmed <sara.ahmed1447@gmail.com>
2024-04-09 08:34:34 -0700

    maintaining-folder-structure

---
+++ src/components/NpmSpeechRecognitionComponent.js
@@ -0,0 +1,44 @@
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+import { useState } from "react";
+
+const NpmSpeechRecognitionComponent = () => {
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 1000,
+  });
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  // Mozilla not supported
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {transcript}
+        </div>
+
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NpmSpeechRecognitionComponent;
